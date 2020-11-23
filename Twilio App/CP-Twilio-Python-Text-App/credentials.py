888809e76f8f71839cfb25fd9484f4531103f131
Nithanth Sivakumar <nithanth9@users.noreply.github.com>
2020-11-22 22:08:38 -0500

    Update credentials.py

--- Twilio App/CP-Twilio-Python-Text-App/credentials.py
+++ Twilio App/CP-Twilio-Python-Text-App/credentials.py
@@ -1,4 +1,4 @@
-account_sid = "AC1624a73f4312373d4dc63d0b4e162886"
-auth_token = "903a4f86734894c0a20c3f620b6992cc"
-my_cell = "+19016476222"
-my_twilio = "+12705440849"
+account_sid = "Your Account Sid"
+auth_token = "Your Auth Token"
+my_cell = "+Your Cell Number"
+my_twilio = "+Your Twilio Number"
